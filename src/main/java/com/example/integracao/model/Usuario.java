cd09aae18de9e87322a5f3bc72d69c5bf0677223
venus <99412095+venous666@users.noreply.github.com>
2023-11-16 09:49:13 -0300

    Add files via upload

---
+++ src/main/java/com/example/integracao/model/Usuario.java
@@ -0,0 +1,16 @@
+package com.example.integracao.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Usuario {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private long cod;
+ private String username;
+ private String password;
+}
